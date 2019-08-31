7af0decde18763fc6b5eb0d47b936d3c97f12407
Amin <toutounchi.ma@gmail.com>
2019-08-31 14:15:57 +0430

    IDE UPDATE

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # this is my customized User Model
 class Member(models.Model):
     base_user = models.OneToOneField(User, on_delete=models.CASCADE)
