c597a4a20b44165bda215e437ad71dd488c83042
Amin <toutounchi.ma@gmail.com>
2019-09-01 11:32:46 +0430

    create basic signup page.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login', views.Login.as_view(), name='login'),
     path('logout', views.Logout.as_view(), name='logout'),
+    path('signup', views.SignUp.as_view(), name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse
+from django.views.generic import CreateView
+from django.contrib.auth import get_user_model
 
 
 class Login(LoginView):
@@ -18,3 +21,11 @@ class Login(LoginView):
 class Logout(LogoutView):
     def get_next_page(self):
         return reverse('home')
+
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+
+    def get_success_url(self):
+        return reverse('accounts:login')
